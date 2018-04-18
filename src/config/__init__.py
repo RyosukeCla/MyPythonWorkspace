c784149e9e98bcc696c8e5d5cd39d03261b5440d
RyosukeCla <ryosukeclarinet@gmail.com>
2018-04-18 16:43:19 +0900

    add config

---
+++ src/config/__init__.py
@@ -0,0 +1,22 @@
+from os import environ
+from os import path
+from dotenv import load_dotenv
+import sys
+from logging import basicConfig, getLogger, DEBUG
+
+basicConfig(level=DEBUG)
+logger = getLogger(__name__)
+
+logger.debug('hello')
+
+dotenv_path = path.join(path.dirname(__file__), '.env0')
+
+if path.exists(dotenv_path):
+  load_dotenv(dotenv_path)
+else:
+  logger.warn('didnt load env file. if you want to load private env, put .env to src/config .')
+
+COMET_ML_API_KEY = environ.get('COMET_ML_API_KEY')
+
+def __main__():
+  print('test init')
