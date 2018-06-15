b277201a209cdef8fdc2cc1158c316e0f3ba717a
RyosukeCla <ryosukeclarinet@gmail.com>
2018-06-15 11:15:38 +0900

    add new Bottoleneck bolock

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -48,6 +48,16 @@ class BottleneckBlock(nn.Module):
   u"""
     Bottoleneck Architecture for ResNet
 
+                    INPUT
+      Conv
+      ReLU
+      Conv                      Shortcut
+      ReLU
+      Conv
+                    ReLU
+                   OUTPUT
+
+
     this makes
       conv2d in_d, oud_d / 4
       conv2d out_d / 4, out_d / 4
@@ -89,6 +99,74 @@ class BottleneckBlock(nn.Module):
     out = self.conv3(out)
     out += self.shortcut(x)
     out = F.relu(out)
+    return out
+
+class BottleneckV2Block(nn.Module):
+  u"""
+    Bottleneck architecture version 2 - improved
+
+                    INPUT
+      BatchNorm
+      ReLU
+      Conv
+      BatchNorm
+      ReLU
+      Conv                  Shortcut
+      BatchNorm
+      ReLU
+      Dropout
+      Conv
+                    Output
+
+    this makes
+      conv2d in_d, oud_d / 4
+      conv2d out_d / 4, out_d / 4
+      conv2d out_d / 4, out_d
+
+    ex) normal bottleneck block
+      conv2d 256 -> 64, with kernel = (1, 1)
+      conv2d 64 -> 64, with kernel = (3, 3)
+      conv2d 64 -> 256, with kernel = (1, 1)
+
+    Args:
+      in_d: input dimention. ex) number of input color channels
+      out_d: output dimention. ex) number of output color channels
+      stride
+
+  """
+  def __init__(self, in_d, out_d, stride=1):
+    super(BottleneckBlock, self).__init__()
+    mid_d = int(out_d / 4)
+    if mid_d < 1:
+      mid_d = 1
+
+    self.bn1 = nn.BatchNorm2d(in_d)
+    self.conv1 = nn.Conv2d(in_d, mid_d, 1, bias=False)
+    self.bn2 = nn.BatchNorm2d(mid_d)
+    self.conv2 = nn.Conv2d(mid_d, mid_d, 3, stride, padding=1, bias=False)
+    self.bn3 = nn.BatchNorm2d(mid_d)
+    self.do = nn.Dropout2d()
+    self.conv3 = nn.Conv2d(mid_d, out_d, 1, bias=False)
+
+    if in_d is out_d:
+      self.shortcut = nn.Sequential()
+    else:
+      self.shortcut = nn.Sequential(
+        nn.Conv2d(in_d, out_d, 1, stride, bias=False)
+      )
+
+  def forward(self, x):
+    out = self.bn1(x)
+    out = F.relu(out)
+    out = self.conv1(out)
+    out = self.bn2(out)
+    out = F.relu(out)
+    out = self.conv2(out)
+    out = self.bn3(out)
+    out = F.relu(out)
+    out = self.do(out)
+    out = self.conv3(out)
+    out += self.shortcut(x)
     return out
 
 class ResNet(nn.Module):
